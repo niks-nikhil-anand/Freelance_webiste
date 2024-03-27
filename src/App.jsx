1dbba303a028b13a1b68bc98897492a498f50f17
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-03-27 17:05:19 +0530

    Done with the hero section Section

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Benefits from './components/Benefits'
+import Collaboration from './components/Collaboration'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <Benefits/>
+      <Collaboration/>
+    </div>
+  )
+}
+
+export default App
