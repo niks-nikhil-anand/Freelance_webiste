7975ebf1b11d4f4318cbcef08ba7395ff9ccd335
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-03-27 23:59:54 +0530

    FIxes some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import {Route , Routes} from 'react-router-dom'
-import Homepage from './Pages/Homepage'
-import ContactUs from './Pages/ContactUs'
+import React from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
+import ContactUs from './Pages/ContactUs';
+import LandingPageLayout from '../src/Layouts/LandingPageLayout.jsx';
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Homepage/>} />
-        <Route path='/contactUs' element={<ContactUs/>} />
+        <Route path="/" element={<LandingPageLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path="about" element={<ContactUs />} />
+        </Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContactUs = () => {
   return (
-    <div className='bg-white-400'>ContactUs</div>
+    <div className='bg-white-400 mt-[300px] mb-[200px]'>ContactUs</div>
   );
 };
 
