6f6a6025e9d2b6e9ef40afa97962c6d4ceeab409
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-03-28 00:05:57 +0530

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { Outlet, Route, Routes } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
 import ContactUs from './Pages/ContactUs';
 import LandingPageLayout from '../src/Layouts/LandingPageLayout.jsx';
+import Services from './Pages/Services.jsx';
+import Projects from './Pages/Projects.jsx';
+import Blogs from './Pages/Blogs.jsx';
+import GetStarted from './Pages/GetStarted.jsx';
 
 const App = () => {
   return (
@@ -11,6 +15,10 @@ const App = () => {
         <Route path="/" element={<LandingPageLayout />}>
           <Route index element={<Homepage />} />
           <Route path="about" element={<ContactUs />} />
+          <Route path="services" element={<Services/>} />
+          <Route path="projects" element={<Projects/>} />
+          <Route path="blogs" element={<Blogs/>} />
+          <Route path="welcome" element={<GetStarted/>} />
         </Route>
       </Routes>
     </div>
