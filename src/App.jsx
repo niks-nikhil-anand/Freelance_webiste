9fb869605e5e29535d13c6528d0e8869e6298282
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-03-27 17:17:38 +0530

    Done with the Roadmap and Footer Sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Benefits from './components/Benefits'
 import Collaboration from './components/Collaboration'
 import Services from './components/Services'
 import Pricing from './components/Pricing'
+import Footer from './components/Footer'
+import Roadmap from './components/Roadmap'
 
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
       <Collaboration/>
       <Services/>
       <Pricing/>
+      <Roadmap/>
+      <Footer/>
     </div>
   )
 }
